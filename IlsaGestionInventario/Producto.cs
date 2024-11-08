180b859eb1a0b4b69edc82e7f29724166417a4cd
Ilsa <ilsasandoval2019@gmail.com>
2024-11-08 16:32:50 -0600

    Atributos de la clase. Metodo para mostrar información.

--- IlsaGestionInventario/Producto.cs
+++ IlsaGestionInventario/Producto.cs
@@ -9,6 +9,17 @@ namespace IlsaGestionInventario
     public class Producto
     {
         public string Nombre { get; set; }
-        public string Precio { get; set; }
+        public decimal Precio { get; set; }
+
+        public Producto(string nombre, decimal precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Producto: {Nombre}, Precio: {Precio:C}");
+        }
     }
 }
